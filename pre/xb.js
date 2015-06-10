61c08c8f979dc86b5c25229ac07d78049291a3d7
Parvez Ahmed <flamboyanz@gmail.com>
2015-06-10 19:41:54 -0400

    added

--- pre/xb.js
+++ pre/xb.js
@@ -43,7 +43,7 @@
 
         this.initOptions(options);
 
-       // this.initialize();
+       this.initialize();
     }
 
     $.extend(Gallery.prototype, {
@@ -275,8 +275,6 @@
 
         initOptions: function (options) {
 
-            console.log('COMES')
-
             var base = this;
 
             // Create a copy of the prototype options:
@@ -302,6 +300,409 @@
 
             base.captionstate = false;
 
+        },
+
+        initialize: function(){
+            this.initStartIndex();
+            if (this.initWidget() === false) {
+                return false;
+            }
+            //this.initEventListeners();
+            // Load the slide at the given index:
+            this.onslide(this.index);
+
+        },
+
+        initWidget: function () {
+
+            var that = this,
+                openHandler = function (event) {
+                    if (event.target === that.container[0]) {
+                        that.container.off(
+                            that.support.transition.end,
+                            openHandler
+                        );
+                        that.handleOpen();
+                    }
+                };
+            this.container = $(this.options.container);
+            if (!this.container.length) {
+                this.console.log(
+                    'xbmod Gallery: Widget container not found.',
+                    this.options.container
+                );
+                return false;
+            }
+            this.slidesContainer = this.container.find(
+                '.carousel-wrapper'
+            )
+
+            if (!this.slidesContainer.length) {
+                this.console.log(
+                    'xbmod Gallery: Slides container not found.',
+                    this.options.slidesContainer
+                );
+                return false;
+            }
+            this.titleElement = this.container.find(
+                this.options.titleElement
+            ).first();
+            if (this.num === 1) {
+                this.container.addClass(this.options.singleClass);
+            }
+            if (this.options.onopen) {
+                this.options.onopen.call(this);
+            }
+            if (this.support.transition && this.options.displayTransition) {
+                this.container.on(
+                    this.support.transition.end,
+                    openHandler
+                );
+            } else {
+                this.handleOpen();
+            }
+            if (this.options.hidePageScrollbars) {
+                // Hide the page scrollbars:
+                this.bodyOverflowStyle = document.body.style.overflow;
+                document.body.style.overflow = 'hidden';
+            }
+            this.container[0].style.display = 'block';
+
+            this.initSlides();
+            //this.initCaptions();
+
+            //this.buildControls();
+            this.container.addClass(this.options.displayClass);
+        },
+
+        initSlides: function (reload) {
+
+            var clearSlides,
+                i;
+            if (!reload) {
+                this.positions = [];
+                this.positions.length = this.num;
+                this.elements = {};
+                this.imagePrototype = document.createElement('img');
+                this.elementPrototype = document.createElement('div');
+                this.slidePrototype = document.createElement('div');
+                $(this.slidePrototype).addClass(this.options.slideClass);
+                //this.slides = this.slidesContainer[0].children;
+                this.slides = []
+                clearSlides = this.options.clearSlides ||
+                    this.slides.length !== this.num;
+            }
+
+
+            this.slideWidth = this.container[0].offsetWidth;
+
+            var containerWidth = document.body.clientWidth;
+
+            this.slideWidth = ( this.slideWidth < document.body.clientWidth ) ? document.body.clientWidth : this.slideWidth;
+
+            this.slideHeight = this.container[0].offsetHeight;
+
+            this.slidesContainer[0].style.width =
+                (this.num * this.slideWidth) + 'px';
+
+            for (i = 0; i < this.num; i += 1) {
+                if (clearSlides) {
+                    this.addSlide(i);
+                }
+
+                this.positionSlide(i);
+
+            }
+            // Reposition the slides before and after the given index:
+            if (this.options.continuous && this.support.transform) {
+
+                this.move(this.circle(this.index - 1), -this.slideWidth, 0);
+                this.move(this.circle(this.index + 1), this.slideWidth, 0);
+            }
+            if (!this.support.transform) {
+                this.slidesContainer[0].style.left =
+                    (this.index * -this.slideWidth) + 'px';
+            }
+
+        },
+
+        addSlide: function (index) {
+            var slide = this.slidePrototype.cloneNode(false);
+            slide.setAttribute('data-index', index);
+            this.slidesContainer[0].appendChild(slide);
+            this.slides.push(slide);
+        },
+
+        positionSlide: function (index) {
+
+            var slide = this.slides[index];
+
+            slide.style.width = this.slideWidth + 'px';
+
+            var tracy = this.index > index ? -this.slideWidth : (this.index < index ? this.slideWidth : 0)
+
+            if (this.support.transform) {
+
+                slide.style.left = (index * -this.slideWidth) + 'px';
+                this.move(index, tracy, 0);
+            }
+
+        },
+
+        onslide: function (index, check) {
+
+            var base = this;
+            this.index = index;
+            this.handleSlide(index);
+        },
+
+        handleSlide: function (index) {
+
+            this.loadElements(index);
+
+        },
+
+        circle: function (index) {
+
+            // Always return a number inside of the slides index range:
+            return (this.num + (index % this.num)) % this.num;
+        },
+
+        move: function (index, dist, speed, status) {
+
+            this.translate(index, dist, speed);
+            this.positions[index] = dist;
+
+        },
+
+        translate: function (index, dist, speed) {
+            console.log(index, dist, speed)
+            var y = 0;
+            var style = this.slides[index].style,
+                transition = this.support.transition,
+                transform = this.support.transform;
+
+            style[transition.name + 'Duration'] = speed + 'ms';
+            style[transform.name] = 'translate(' + dist + 'px, ' + y + 'px)' +
+                (transform.translateZ ? ' translateZ(0)' : '');
+
+        },
+
+        imageFactory: function (obj, callback) {
+
+            var that = this,
+                img = this.imagePrototype.cloneNode(false),
+                url = obj.dataset.src,
+                backgroundSize = this.options.stretchImages,
+                called,
+                element,
+                callbackWrapper = function (event) {
+                    if (!called) {
+                        event = {
+                            type: event.type,
+                            target: element
+                        };
+                        if (!element.parentNode) {
+                            // Fix for IE7 firing the load event for
+                            // cached images before the element could
+                            // be added to the DOM:
+                            return that.setTimeout(callbackWrapper, [event]);
+                        }
+                        called = true;
+                        $(img).off('load error', callbackWrapper);
+                        if (backgroundSize) {
+                            if (event.type === 'load') {
+                                element.style.background = 'url("' + url +
+                                    '") center no-repeat';
+                                element.style.backgroundSize = backgroundSize;
+                            }
+                        }
+
+                    }
+                },
+                title;
+
+            /*if (typeof url !== 'string') {
+
+             url = this.getItemProperty(obj[0], 'data-src');
+             //caption = obj[1].innerHTML;
+             title = this.getItemProperty(obj[0], this.options.titleProperty);
+
+             }*/
+            if (backgroundSize === true) {
+                backgroundSize = 'contain';
+            }
+            backgroundSize = this.support.backgroundSize &&
+                this.support.backgroundSize[backgroundSize] && backgroundSize;
+            if (backgroundSize) {
+                element = this.elementPrototype.cloneNode(false);
+            } else {
+                element = img;
+                img.draggable = false;
+            }
+            if (title) {
+                element.title = title;
+            }
+            $(img).on('load error', callbackWrapper);
+
+
+            img.src = url;
+            return element;
+        },
+
+        createCaptionElement: function(obj){
+
+            var that = this,
+                txt = obj.innerHTML,
+                element = this.elementPrototype.cloneNode(false),
+                p = document.createElement('p').cloneNode(false);
+            //textNode = document.createTextNode(txt)
+
+
+            p.innerHTML = txt
+            element.appendChild(p)
+            element.className = 'gallerycaption';
+
+            return element;
+
+        },
+
+
+        createElement: function (obj, callback) {
+
+            var type = obj && this.getItemProperty(obj, 'data-src');
+
+            var factory = (type && this[type.split('/')[0] + 'Factory']) || this.imageFactory,
+                element = obj && factory.call(this, obj, callback);
+            if (!element) {
+                element = this.elementPrototype.cloneNode(false);
+                this.setTimeout(callback, [{
+                    type: 'error',
+                    target: element
+                }]);
+            }
+            $(element).addClass(this.options.slideContentClass);
+
+            return element;
+        },
+
+        loadElement: function (index) {
+
+
+            if (!this.elements[index]) {
+
+                if (this.slides[index].children[1]) {
+
+                    this.elements[index] = $(this.slides[index])
+                        .hasClass(this.options.slideErrorClass) ? 3 : 2;
+                } else {
+
+                    this.elements[index] = 1; // Loading
+                    $(this.slides[index]).addClass(this.options.slideLoadingClass);
+
+                    var insertNode = this.list[index].children[0];
+                    var captionNode = this.slides[index].children[0];
+
+                    /*this.slides[index].appendChild(this.createElement(
+                     this.list[index].children[0],
+                     this.proxyListener
+                     ));*/
+
+                    this.slides[index].insertBefore(this.createElement(
+                        insertNode,
+                        this.proxyListener
+                    ), captionNode)
+
+
+
+                }
+            }
+        },
+
+        loadElements: function (index) {
+
+            // preloadRange * 2 + 1
+            var limit = Math.min(this.num, this.options.preloadRange + 1),
+                j = index,
+                i;
+            for (i = 0; i < limit; i += 1) {
+                // First load the current slide element (0),
+                // then the next one (+1),
+                // then the previous one (-2),
+                // then the next after next (+2), etc.:
+                j += i * (i % 2 === 0 ? -1 : 1);
+                // Connect the ends of the list to load slide elements for
+                // continuous navigation:
+                j = this.circle(j);
+
+                this.loadElement(j);
+            }
+        },
+
+        getNodeIndex: function (element) {
+            return parseInt(element.getAttribute('data-index'), 10);
+        },
+
+        getNestedProperty: function (obj, property) {
+            property.replace(
+                // Matches native JavaScript notation in a String,
+                // e.g. '["doubleQuoteProp"].dotProp[2]'
+                /\[(?:'([^']+)'|"([^"]+)"|(\d+))\]|(?:(?:^|\.)([^\.\[]+))/g,
+                function (str, singleQuoteProp, doubleQuoteProp, arrayIndex, dotProp) {
+                    var prop = dotProp || singleQuoteProp || doubleQuoteProp ||
+                        (arrayIndex && parseInt(arrayIndex, 10));
+                    if (str && obj) {
+                        obj = obj[prop];
+                    }
+                }
+            );
+            return obj;
+        },
+
+        getDataProperty: function (obj, property) {
+            if (obj.getAttribute) {
+                var prop = obj.getAttribute('data-' +
+                    property.replace(/([A-Z])/g, '-$1').toLowerCase());
+                if (typeof prop === 'string') {
+                    if (/^(true|false|null|-?\d+(\.\d+)?|\{[\s\S]*\}|\[[\s\S]*\])$/
+                            .test(prop)) {
+                        try {
+                            return $.parseJSON(prop);
+                        } catch (ignore) {}
+                    }
+                    return prop;
+                }
+            }
+        },
+
+        getItemProperty: function (obj, property) {
+            var prop = obj[property];
+            if (prop === undefined) {
+                prop = this.getDataProperty(obj, property);
+                if (prop === undefined) {
+                    prop = this.getNestedProperty(obj, property);
+                }
+            }
+            return prop;
+        },
+
+        initStartIndex: function () {
+            var index = this.options.index,
+                urlProperty = this.options.urlProperty,
+                i;
+            // Check if the index is given as a list object:
+            if (index && typeof index !== 'number') {
+                for (i = 0; i < this.num; i += 1) {
+                    if (this.list[i] === index ||
+                        this.getItemProperty(this.list[i], urlProperty) ===
+                        this.getItemProperty(index, urlProperty)) {
+                        index  = i;
+                        break;
+                    }
+                }
+            }
+            // Make sure the index is in the list range:
+            this.index = this.circle(parseInt(index, 10) || 0);
         }
 
     });
